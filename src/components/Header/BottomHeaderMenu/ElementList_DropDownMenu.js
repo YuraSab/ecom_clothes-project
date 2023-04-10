d7b525c39d970e49b12598f470a74e34f02f1bec
Yura Sabat <yrasabat@gmail.com>
2023-04-10 16:56:19 +0300

    Drop menu - part two

---
+++ src/components/Header/BottomHeaderMenu/ElementList_DropDownMenu.js
@@ -0,0 +1,277 @@
+export const DropMenuList = [
+    {
+        // MALE
+        //male / female
+        gender: "male",
+        // clothes, shoes, accessories etc
+        typeOfCloth: [
+            {
+                name: "clothes",
+                categories: [
+                    // shorts, t-shirts, thousands etc
+                    // ВЕРХНІЙ ОДЯГ
+                    {
+                        name: "ВЕРХНІЙ ОДЯГ",
+                        link: "",
+                        type: "category"
+                    },
+                    {
+                        name: "ВІТРОВКИ, КУРТКИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "ПАРКИ ОСІНЬ-ВЕСНА",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "ЗИМОВІ КУРТКИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "СВІТШОТИ, ТОЛСТОВКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ШТАНИ
+                    {
+                        name: "ШТАНИ",
+                        link: "",
+                        type: "category"
+                    },
+                    {
+                        name: "ДЖИНСИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "СПОРТИВНІ ШТАНИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "КАРГО, ДЖОГЕРИ, ЧИНОСИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "СПОРТИВНІ КОСТЮМИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ПОЛО ТА ФУТБОЛКИ
+                    {
+                        name: "ПОЛО ТА ФУТБОЛКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ШОРТИ
+                    {
+                        name: "ШОРТИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ЛІТНІ КОМПЛЕКТИ
+                    {
+                        name: "ЛІТНІ КОМПЛЕКТИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // БІЛИЗНА
+                    {
+                        name: "БІЛИЗНА",
+                        link: "",
+                        type: "category"
+                    },
+                ]
+            },
+            // shoes
+            {
+                name: "shoes",
+                categories: [
+                    // shorts, t-shirts, thousands etc
+                    {
+                        name: "ЗИМОВЕ ВЗУТТЯ",
+                        link: "",
+                        type: "category"
+                    },
+                    {
+                        name: "КРОСІВКИ, КЕДИ",
+                        link: "",
+                        type: "category"
+                    },
+                ]
+            },
+            // bags
+            {
+                name: "backpacks_and_bags",
+                categories: [
+                    // РЮКЗАКИ
+                    {
+                        name: "РЮКЗАКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // СУМКИ НА ПОЯС
+                    {
+                        name: "СУМКИ НА ПОЯС",
+                        link: "",
+                        type: "category"
+                    },
+                    // for free spacing
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    // СУМКИ ЧЕРЕЗ ПЛЕЧЕ
+                    {
+                        name: "СУМКИ ЧЕРЕЗ ПЛЕЧЕ",
+                        link: "",
+                        type: "category"
+                    },
+                ]
+            },
+            // accessories
+            {
+                name: "backpacks_and_bags",
+                categories: [
+                    // КЕПКИ, ПАНАМИ
+                    {
+                        name: "КЕПКИ, ПАНАМИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ЗИМОВІ ШАПКИ
+                    {
+                        name: "ЗИМОВІ ШАПКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    // ШАРФИ, РУКАВИЧКИ
+                    {
+                        name: "ШАРФИ, РУКАВИЧКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ШКАРПЕТКИ
+                    {
+                        name: "ШКАРПЕТКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    {name: "", link: "", type: "category"},
+                    // РЕМЕНІ, ОКУЛЯРИ, СТІКЕРИ, ГАМАНЦІ
+                    {
+                        name: "РЕМЕНІ, ОКУЛЯРИ, СТІКЕРИ, ГАМАНЦІ",
+                        link: "",
+                        type: "category"
+                    },
+                ]
+            },
+        ]
+    },
+
+    // FEMALE
+
+    {
+        //male / female
+        gender: "male",
+        // clothes, shoes, accessories etc
+        typeOfCloth: [
+            {
+                name: "clothes",
+                categories: [
+                    // shorts, t-shirts, thousands etc.
+                    // ВЕРХНІЙ ОДЯГ
+                    {
+                        name: "ВЕРХНІЙ ОДЯГ",
+                        link: "",
+                        type: "category"
+                    },
+                    {
+                        name: "ВІТРОВКИ, КУРТКИ",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    {
+                        name: "ПАЛЬТО",
+                        link: "",
+                        type: "subcategory"
+                    },
+                    // ТОЛСТОВКИ, СВІТШОТИ, КОФТИ
+                    {
+                        name: "ТОЛСТОВКИ, \n СВІТШОТИ, КОФТИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // БОДІ
+                    {
+                        name: "БОДІ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ШТАНИ
+                    {
+                        name: "ШТАНИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ТОПИ І ФУТБОЛКИ
+                    {
+                        name: "ТОПИ І ФУТБОЛКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // СПОРТИВНІ КОСТЮМИ
+                    {
+                        name: "СПОРТИВНІ КОСТЮМИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ЛІТНІ КОМПЛЕКТИ
+                    {
+                        name: "ЛІТНІ КОМПЛЕКТИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ПЛАТТЯ
+                    {
+                        name: "ПЛАТТЯ",
+                        link: "",
+                        type: "category"
+                    },
+                    // ШОРТИ
+                    {
+                        name: "ШОРТИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // КУПАЛЬНИКИ
+                    {
+                        name: "КУПАЛЬНИКИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // КОСТЮМИ
+                    {
+                        name: "КОСТЮМИ",
+                        link: "",
+                        type: "category"
+                    },
+                    // БІЛИЗНА
+                    {
+                        name: "БІЛИЗНА",
+                        link: "",
+                        type: "category"
+                    },
+                ]
+            },
+        ]
+    }
+];
